443d19fdabcc67ff4d4016998d031c4f6d8df9cc
Axel Tamir <axeltamir@Axels-MacBook-Air-1006.local>
2026-08-08 19:37:00 -0700

    added a shit ton of essays

--- data.js
+++ data.js
@@ -19,7 +19,17 @@ const MOVIES = [
 ];
 
 const ESSAYS = [
-	{ title: "Fighting Our Fantasies: Dark City and the Politics of Psychoanalysis", meta: "Todd Mcgowan", note: "In 'Lacan and Contemporary Film'"}
+	{ title: "Fighting Our Fantasies: Dark City and the Politics of Psychoanalysis", meta: "Todd Mcgowan", note: "In 'Lacan and Contemporary Film'"},
+	{ title: "Woman is One of the Names-of-the-Father, or How Not to Misread Lacan's Formulas of Sexuation", meta: "Slavoj Zizek", link: "https://www.lacan.com/zizwoman.htm"},
+	{ title: "The Big Other doesn't exist", meta: "Slavoj Zizek", link: "https://www.psychomedia.it/jep/number5/zizek.htm"},
+	{ title: "PSYCHOANALYSIS AND POST-MARXISM: The Case of Alain Badiou", meta: "Slavoj Zizek", link: "https://www.lacan.com/zizek-badiou.htm"},
+	{ title: "Too Much Democracy?", meta: "Slavoj Zizek", link: "https://www.lacan.com/toomuch.htm"},
+	{ title: "Christians, Jews, and Other Criminals: A Critique of Jean-Claude Milner", meta: "Slavoj Zizek", link: "https://www.lacan.com/milner.htm"},
+	{ title: "The Obscenity of Human Rights: Violence as a Sympton", meta: "Slavoj Zizek", link: "https://www.lacan.com/zizviol.htm"},
+	{ title: "Mamdani's American Dream", meta: "Slavoj Zizek", link: "https://www.project-syndicate.org/commentary/mamdani-american-dream-july-4-address-versus-maga-narrative-by-slavoj-zizek-2026-07?a_la=english&a_d=6a4e45b93d2251523a8c4937&a_m=&a_a=click&a_s=&a_p=/columnist/slavoj-zizek&a_li=mamdani-american-dream-july-4-address-versus-maga-narrative-by-slavoj-zizek-2026-07&a_pa=columnist-commentaries&a_ps=&a_ms=&a_r="},
+	{ title: "When Communism is the Only Option", meta: "Slavoj Zizek", link: "https://www.project-syndicate.org/commentary/iran-water-crisis-survival-requires-state-control-solidarity-international-cooperation-by-slavoj-zizek-2025-12?a_la=english&a_d=6939836db214a22f5e74c4c8&a_m=&a_a=click&a_s=&a_p=%2Fcolumnist%2Fslavoj-zizek&a_li=iran-water-crisis-survival-requires-state-control-solidarity-international-cooperation-by-slavoj-zizek-2025-12&a_pa=columnist-commentaries&a_ps=&a_ms=&a_r="},
+
+
 ];
 
 const BOOKS = [
